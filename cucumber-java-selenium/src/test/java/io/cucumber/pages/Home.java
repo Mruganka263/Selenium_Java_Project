c32baa84d0e3be90da6e11465f8ceb68336b703b
mruganka <mruganka.263@gmail.com>
2025-03-12 18:41:45 +0000

    Added new steps

--- cucumber-java-selenium/src/test/java/io/cucumber/pages/Home.java
+++ cucumber-java-selenium/src/test/java/io/cucumber/pages/Home.java
@@ -14,6 +14,47 @@ public class Home extends Page {
   @FindBy(css = "h1")
   private WebElement title;
 
+  @FindBy(css ="[href*='abtest']")
+  private  WebElement ABTestLink;
+  @FindBy(css = "h3")
+  private WebElement ABTestControlHeading;
+
+  @FindBy(css ="[href='/add_remove_elements/']")
+  private WebElement addRemoveLink;
+
+  @FindBy(css ="[href='/basic_auth']")
+  private WebElement basicAuthLink;
+
+  @FindBy(css ="[href='/broken_images']")
+  private WebElement brokenImagesLink;
+
+  @FindBy(css ="[href='/challenging_dom']")
+  private WebElement challengingDomLink;
+
+  @FindBy(css ="[href='/checkboxes']")
+  private WebElement checkboxesLink;
+
+  @FindBy(css ="[href='/context_menu']")
+  private WebElement contextMenu;
+
+  @FindBy(css="[href='/disappearing_elements']")
+  private WebElement disapperingElement;
+
+  @FindBy(css="[href='/drag_and_drop']")
+  private WebElement dragAndDrop;
+
+  @FindBy(css="[href='/dropdown']")
+  private WebElement dropdown;
+
+  @FindBy(css="[href='/dynamic_content']")
+  private WebElement dynamicContent;
+
+  @FindBy(css="[href=\"/dynamic_controls\"]")
+  private WebElement dynamicControl;
+
+  @FindBy(css="[href=\"/dynamic_loading\"]")
+  private WebElement dynamicLoading;
+
   public WebElement getTitle() {
     return title;
   }
@@ -22,4 +63,55 @@ public class Home extends Page {
     driver.navigate().refresh();
     System.out.println("Refreshed page");
   }
+
+  public void click(String link){
+    switch (link) {
+      case "A/B Testing":
+        ABTestLink.click();
+        break;
+      case "Add/Remove Elements":
+        addRemoveLink.click();
+        break;
+      case "Basic Auth":
+        basicAuthLink.click();
+        break;
+      case "Broken Images":
+        brokenImagesLink.click();
+        break;
+      case "Challenging DOM":
+        challengingDomLink.click();
+        break;
+      case "Checkboxes":
+        checkboxesLink.click();
+        break;
+      case "Context Menu":
+        contextMenu.click();
+        break;
+      case "Disappearing Elements":
+        disapperingElement.click();
+        break;
+      case "Drag and Drop":
+        dragAndDrop.click();
+        break;
+      case "Dropdown":
+        dropdown.click();
+        break;
+      case "Dynamic Content":
+        dynamicContent.click();
+        break;
+      case "Dynamic Controls":
+        dynamicControl.click();
+        break;
+      case "Dynamic Loading":
+        dynamicLoading.click();
+        break;
+      default:
+        System.out.println("Error");
+    }
+  }
+
+  public WebElement getABTestControlHeadingWebEle(){
+    return this.ABTestControlHeading;
+  }
+
 }
