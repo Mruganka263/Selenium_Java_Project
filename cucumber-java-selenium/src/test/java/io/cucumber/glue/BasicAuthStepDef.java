ac71646236dc803f911a4ead585961a0fd5e271e
mruganka <mruganka.263@gmail.com>
2025-03-12 18:35:15 +0000

    Basic Authorization ex. code

---
+++ cucumber-java-selenium/src/test/java/io/cucumber/glue/BasicAuthStepDef.java
@@ -0,0 +1,37 @@
+package io.cucumber.glue;
+
+import io.cucumber.core.Context;
+import io.cucumber.core.Manager;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.pages.BasicAuth;
+import io.cucumber.util.CommonUtil;
+import org.testng.Assert;
+
+import java.io.IOException;
+
+public class BasicAuthStepDef extends Context {
+
+    public BasicAuthStepDef(Manager manager) {
+        super(manager);
+    }
+    BasicAuth ba = new BasicAuth(manager.getDriver());
+
+    @And("valid credentials are supplied")
+    public void validCredentialsAreSupplied() throws IOException {
+        String currentURL= manager.getDriver().getCurrentUrl();
+        String sb = currentURL;
+        sb= sb.substring(0,8)+
+                CommonUtil.getConfigProperty("user")+
+                ":"+CommonUtil.getConfigProperty("password")+"@"
+                +sb.substring(8);
+        System.out.println(sb);
+        manager.getDriver().get(sb);
+    }
+
+    @Then("Congratulations should be displayed")
+    public void congratulationsShouldBeDisplayed() {
+
+        Assert.assertTrue(ba.getMessage().contains("Congratulations"),"Not Authorized");
+    }
+}
---
+++ cucumber-java-selenium/src/test/java/io/cucumber/pages/BasicAuth.java
@@ -0,0 +1,20 @@
+package io.cucumber.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class BasicAuth extends Page{
+
+    public BasicAuth(ChromeDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css=".example p")
+    private WebElement message;
+
+    public String getMessage() {
+        return message.getText();
+    }
+
+}
