5d274b63bd733a0418b650aa05501b5af29d4078
mruganka <mruganka.263@gmail.com>
2025-03-13 19:58:46 +0000

    Compare List function added

--- cucumber-java-selenium/src/test/java/io/cucumber/util/CommonUtil.java
+++ cucumber-java-selenium/src/test/java/io/cucumber/util/CommonUtil.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Properties;
+import java.util.*;
 
 public class CommonUtil {
 
@@ -21,10 +21,22 @@ public class CommonUtil {
             return 500; // Assuming error
         }
     }
+
     public static String getConfigProperty(String str) throws IOException {
         Properties prop = new Properties();
         InputStream in = new FileInputStream("src/test/resources/config.properties");
         prop.load(in);
         return prop.getProperty(str);
     }
+
+    public static boolean areListsEqual(List<Map<String, String>> list1, List<Map<String, String>> list2) {
+        if (list1.size() != list2.size()) {
+            return false; // Different sizes, not equal
+        }
+        Set<Map<String, String>> set1 = new HashSet<>(list1);
+        Set<Map<String, String>> set2 = new HashSet<>(list2);
+
+        return set1.equals(set2);
+    }
+
 }
